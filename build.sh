8994988f328f0b23b97adefabf95042e3c9a6545
Ernad Husremovic <hernad@bring.out.ba>
2013-03-25 22:56:21 +0100

    run

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ GYP_GENERATORS=ninja gyp hello.gyp --toplevel-dir=`pwd` --depth=0
 echo http://n8.io/converting-a-c-library-to-gyp/
 
 ninja -v -C out/Default/ all
+
+echo "---run----"
+out/Default/h1
